8e9c7a8e555a1c758e4c898c2f16b9044e610d90
EskalarteDexter <88998149+EskalarteDexter@users.noreply.github.com>
2023-06-17 06:58:27 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -118,19 +118,6 @@ echo -e "[ ${green}INFO${NC} ] Allright good ... installation file is ready"
 sleep 2
 echo -ne "[ ${green}INFO${NC} ] Check permission : "
 
-PERMISSION
-if [ -f /home/needupdate ]; then
-red "Your script need to update first !"
-exit 0
-elif [ "$res" = "Permission Accepted..." ]; then
-green "Permission Accepted!"
-else
-red "Permission Denied!"
-rm setup.sh > /dev/null 2>&1
-sleep 10
-exit 0
-fi
-sleep 3
 
 mkdir -p /etc/ssnvpn
 mkdir -p /etc/ssnvpn/theme
